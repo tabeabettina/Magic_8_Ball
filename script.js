193a906e0f4c485fd1feae4425b5447ba4cf0168
melissagoe <melissa.goebel@stud.fhgr.ch>
2025-05-07 19:09:24 +0200

    update

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const container = document.querySelector ("#answer-container");
-
 const API_URL = 'https://answerbook.david888.com/?lang=en';
+
+const myData = await fetchData(API_URL);
+console.log (myData);
+
+
+// let zahl = 200;
+// let text1 = "Melissa" + zahl;
+// console.log(text1);
+// let text2 = `Melissa ${zahl};`
+// console.log (text2);
+
+
+showData();
 
 async function fetchData (url) {
     try {
         const response = await fetch(url);
-        return await response.json();
+        let jsObject = await response.json();
+        return jsObject;
     } catch (e) {
         console.error(e);
         return [];
@@ -13,17 +26,11 @@ async function fetchData (url) {
     }
 }
 
-const myData = await fetchData(API_URL);
 
 function showData (){
-
-        let card = document.createElement("article");
-        card.classList.add("card");
-        card.innerHTML = `
-        <h2>${myData.answer}</h2>
-        `;
-        container.appendChild(card);
-
+        console.log(myData.answer);
+        // let card = document.createElement("article");
+        // card.classList.add("card");
+        // card.innerHTML = `<h2>${myData.answer}</h2>`;
+        // container.appendChild(card);
 }
-
-showData();
